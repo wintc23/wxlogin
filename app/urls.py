ce49f8058443c7d9a2f55563e32d92e7c1008f9b
lushg <lushg-tcxg@qq.com>
2018-01-04 11:44:30 +0000

    fix redirect url

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ app_name="app"
 
 urlpatterns=[
      path("login",views.login,name="login"),
-     path("weixin/",views.login_callback,name="login_callback"),
+     path("login/weixin/",views.login_callback,name="login_callback"),
      path("index/",views.index,name="index"),
      path("",views.home,name="home"),
  ]
